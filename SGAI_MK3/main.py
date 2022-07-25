1f43f506896f69aa56822b7240669d010ed7d616
Alex_A <77032042+crazininja@users.noreply.github.com>
2022-07-25 14:06:52 -0400

    Update main.py

--- SGAI_MK3/main.py
+++ SGAI_MK3/main.py
@@ -1,7 +1,13 @@
+<<<<<<< Updated upstream
+=======
+from ast import If
+import sys
+>>>>>>> Stashed changes
 import pygame
 from Board import Board
 import PygameFunctions as PF
 import random as rd
+<<<<<<< Updated upstream
 
 # Constants
 ROWS = 6
@@ -10,6 +16,25 @@ BORDER = 150                    # Number of pixels to offset grid to the top-lef
 CELL_DIMENSIONS = (100,100)     # Number of pixels (x,y) for each cell
 ACTION_SPACE = ["moveUp", "moveDown", "moveLeft", "moveRight", "heal", "bite"]
 SELF_PLAY = False
+=======
+import copy
+from ExitPoint import ExitPoint
+
+# Constants
+HUMAN_PLAY = True
+SHOW_EVERY_FRAME = False       # Will show each action taken by AI if True. Shows only last frame if False.
+ROWS = 30
+COLUMNS = 30
+OFFSET = 50                    # Number of pixels to offset grid to the top-left side
+CELL_DIMENSIONS = 20           # Number of pixels for each cell
+DAYS_TO_DEATH = 100            # The number of days until there is a 50% chance of death
+SHOW_EPSILON_GRAPH = False
+USE_STATE_QTABLE = False
+EXIT_POINTS = 3 #Number of exit points
+
+if not HUMAN_PLAY:
+    rd.seed(1)
+>>>>>>> Stashed changes
 
 # Player role variables
 player_role = "Government"      # Valid options are "Government" and "Zombie"
@@ -19,6 +44,14 @@ roleToRoleBoolean = {"Government": False, "Zombie": True}
 #Create the game board
 GameBoard = Board((ROWS,COLUMNS), BORDER, CELL_DIMENSIONS, roleToRoleNum[player_role])
 GameBoard.populate()
+
+#create exit points
+ExitPoints = [] #create empty list to store exitpoints in 
+for i in range(EXIT_POINTS): #create the amountspecified
+    NewExit = ExitPoint((rd.randint(0, int(ROWS * COLUMNS) - 1))) #create the exit point to be added, give a random location
+    ExitPoints.append(NewExit) #add to the list
+
+#"ExitPoints" will be the list that is iterated through at the end of each round to check if anything is in exit
 
 # Self play variables
 alpha = 0.1
@@ -180,6 +213,7 @@ while running:
             old_qval = b
             old_state = i
             
+<<<<<<< Updated upstream
             # Update
             # Q(S, A) = Q(S, A) + alpha[R + gamma * max_a Q(S', A) - Q(S, A)]
             reward = GameBoard.act(old_state, action_to_take)
@@ -222,3 +256,81 @@ while running:
                 print("loseCase")
             if event.type == pygame.QUIT:
                 running = False
+=======
+            # If the player or AI has selected an action, then the simulation can advance one step
+            if player_moved:   
+                #oldGameboard = copy.deepcopy(GameBoard)
+                
+                # Implement the player's action
+                # Doesn't check for "pass" since nothing needs to change
+                if player_action[0] == "move":
+                    GameBoard.move(player_action[1], player_loc, True)
+                elif player_action[0] == "vaccinate":
+                    GameBoard.vaccinate(player_action[1], player_loc)
+
+                # Allow all the people in the simulation to have a turn now
+                PF.simulate(GameBoard)
+
+                # People die!
+                PF.progress_infection(GameBoard, DAYS_TO_DEATH)
+
+                # If the AI is playing, then implement reinforcement learning
+                if not HUMAN_PLAY:
+                    if USE_STATE_QTABLE:
+                        # Figure out the reward for the action selected
+                        #reward = PF.reward(oldGameboard, GameBoard, player_action)
+                        reward = PF.reward(GameBoard, GameBoard, player_action)
+                        # Update the Q-Table
+                        # player_ind is the location of the player at the old location
+                        QTable[player_ind][choice] = PF.update_Q_value(
+                            QTable[player_ind][choice],
+                            alpha,
+                            reward,
+                            gamma,
+                            max(QTable[GameBoard.state[GameBoard.govt_index].location])
+                        )
+                    else:
+                        reward = PF.reward2(player_action, GameBoard)
+                        new = GameBoard.sense_nearby()
+                        QTable2[l[0]][l[1]][l[2]][l[3]][choice] = PF.update_Q_value(
+                            QTable2[l[0]][l[1]][l[2]][l[3]][choice],
+                            alpha,
+                            reward,
+                            gamma,
+                            max(QTable2[new[0]][new[1]][new[2]][new[3]]))
+                
+                #Check if any people are in the exitpoint
+                AmountExited = 0 #create a Variable to store how many people exited this round
+                for Exit in ExitPoints: #iterate thtough all exit points
+                    AmountExited += Exit.CheckPeopleExited(GameBoard.people) #check if people exited; if someone was in exit, returns 1, if not, returns 0
+
+                # Check for end conditions
+                if GameBoard.num_infected() == 0:   # There are no infected people left
+                    #if HUMAN_PLAY or episodes_ran % 100 == 0 or episodes_ran == episodes:
+                    PF.run(GameBoard, episodes_ran)
+                    PF.display_finish_screen()
+                    survivors.append(GameBoard.population)
+                    running = False
+                
+                #del oldGameboard
+                
+    # Store the current conditions
+    epsilon_list.append(epsilon)
+    survivor_list.append(sum(survivors) / len(survivors))
+
+#print(QTable2)
+
+print(QTable2['X']['V']['E']['E'])
+#for a in possible_entries:
+#    for b in possible_entries:
+#        for c in possible_entries:
+#            print(QTable2[a][b][c]['X'][4])
+
+print(f"Mean # of surviving people was {sum(survivors) / len(survivors)}.")
+
+if SHOW_EPSILON_GRAPH:
+    ax.plot(epsilon_list, survivor_list)
+    plt.show()
+else:
+    input("Press Enter to continue.")
+>>>>>>> Stashed changes

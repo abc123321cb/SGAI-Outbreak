b59bcf56ceeab36d29975a36763a4d3f5b269688
Steven Pereanu <pereanusteven@gmail.com>
2022-07-25 14:50:26 -0400

    Fixed the problem kinda badly but it works

--- SGAI_MK3/ExitPoint.py
+++ SGAI_MK3/ExitPoint.py
@@ -6,9 +6,9 @@ class ExitPoint:
     def CheckPeopleExited(self, PersonSet, GameBoard):
         PersonExited = 0 #setting a counter of people who have exited
         for Person in PersonSet: #iterating through each person in the set
-            if Person.location == self.location and Person.isInfected != True: #check if they're not infected and are in the exit point
+            if Person != None and Person.location == self.location and Person.isInfected != True: #check if they're not infected and are in the exit point
                 PersonExited += 1 #add to counter
-                GameBoard.Death(Person.location, Person.index) #"death" event to remove them from board
+                GameBoard.death(Person.location, Person.index) #"death" event to remove them from board
                 return PersonExited #return the amount, early return since no one else could be in the tile
                 #only one person can be at the tile at once, so there is no need to remove it 
         return PersonExited #return the amount

ddeefb8789434fd0aa9326d3bfc7c08150373aa9
Alex_A <77032042+crazininja@users.noreply.github.com>
2022-07-25 10:29:47 -0400

    Create ExitPoint.py

---
+++ SGAI_MK3/ExitPoint.py
@@ -0,0 +1,18 @@
+class ExitPoint:
+    def __init__(self, this_index, location):
+        self.index = this_index #assigning it an index in case we want multiple exit points in future
+        self.location = location #assigning location; exit point does not need 'conditions'
+    
+    def CheckPeopleExited(self, PersonSet, GameBoard):
+        Amount = 0 #setting a counter of people who have exited
+        for Person in PersonSet: #iterating through each person in the set
+            if Person.location == self.location and Person.isInfected != True: #check if they're not infected and are in the exit point
+                Amount += 1 #add to counter
+                GameBoard.Death(Person.location, Person.index) #"death" event to remove them from board
+        return Amount #return the amount
+ 
+
+
+            
+    
+
